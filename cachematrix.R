eb8687eefe93b676f83a4fe37ee5435462702cfc
Sam Ipsen <sips@microsoft.com>
2016-11-03 16:22:28 -0700

    Literally did the whole assignment (in one commit). Note to self "Actually use version contro!l"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Sam Ipsen
+## Programming Assignment 2: Lexical Scoping
+## 11/3/2016
+## Creates a manages a Matrix with a cache for the Inverse
 
-## Write a short comment describing this function
 
+## Creates a 'special' matrix with getters and setters for
+## the internal matrix and it's cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_cache <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_cache <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inv_cache <<- inv
+  getInverse <- function() inv_cache
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of the provide matrix
+## If the cache is set, it will return the cache
+## otherwise it will calculate the inverse, set the cache 
+## and return the result
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv
 }
